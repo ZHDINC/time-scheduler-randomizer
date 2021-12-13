a932f1f988cb5c8900c4d576bcdb5cb149538122
ZHDINC <ZHDINC@gmail.com>
2021-12-12 22:19:03 -0500

    Basic Scheduling implemented

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include<string>
 #include<vector>
 #include<filesystem>
+#include<random>
 
 class ScheduleItem
 {
@@ -11,6 +12,7 @@ class ScheduleItem
     int duration;
 public:
     ScheduleItem(std::string category, std::string title, int duration) : category{category}, title{title}, duration{duration} {}
+    ScheduleItem() {};
     ~ScheduleItem() {};
 
     std::string Category() const { return category; }
@@ -57,7 +59,7 @@ int main()
     std::string filename{".\\schedule.txt"};
     std::ifstream f;
     std::vector<ScheduleItem> list;
-    std::cout << "Current path is " << std::filesystem::current_path() << '\n';
+    // std::cout << "Current path is " << std::filesystem::current_path() << '\n';
     std::string str{};
     f.open(filename);
     if(!f.is_open())
@@ -69,7 +71,7 @@ int main()
     int currDur;
     while(std::getline(f, str))
     {
-        std::cout << "Fetching...\n";
+        // std::cout << "Fetching...\n";
         if(str == "=" || str == "")
         {
             fetchCategoryTitle = true;
@@ -84,12 +86,43 @@ int main()
         }
         std::vector<std::string> lineItemList = LineItemParser(str);
         ScheduleItem tempItem{currCat, lineItemList[0], stoi(lineItemList[1])};
-        std::cout << "Temporary Item: " << tempItem.Category() << ", " << tempItem.Title() << ", " << tempItem.Duration() << " minutes\n";
+        // std::cout << "Temporary Item: " << tempItem.Category() << ", " << tempItem.Title() << ", " << tempItem.Duration() << " minutes\n";
         list.push_back(tempItem);
     }
 
-    for(ScheduleItem s : list)
+    // for(ScheduleItem s : list)
+    // {
+    //     std::cout << s.Category() << ", " << s.Title() << ", " << s.Duration() << " minutes\n";
+    // }
+
+    int hourStart, minuteStart;
+    std::string startTimeFull;
+    int hourEnd, minuteEnd;
+    bool printingSchedule = true;
+    std::cout << "Start time: ";
+    std::cin >> hourStart >> minuteStart;
+    std::cout << "End time: ";
+    std::cin >> hourEnd >> minuteEnd;
+    std::cout << "Inputted times: Start: " << hourStart << ":" << minuteStart << " End: " << hourEnd << ":" << minuteEnd << "\n";
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> distrib(0, list.size() - 1);
+    while(printingSchedule)
     {
-        std::cout << s.Category() << ", " << s.Title() << ", " << s.Duration() << " minutes\n";
+        ScheduleItem current = list[distrib(gen)];
+        std::cout << hourStart << ":" << minuteStart << " " << current.Title() << '\n';
+        minuteStart += current.Duration();
+        if(minuteStart >= 60 && (hourStart != hourEnd))
+        {
+            minuteStart -= 60;
+            hourStart++;
+        }
+        if(hourStart == hourEnd)
+        {
+            if(minuteStart >= minuteEnd)
+            {
+                printingSchedule = false;
+            }
+        }
     }
 }
